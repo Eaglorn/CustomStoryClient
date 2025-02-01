98c170ee88a03ace7a57ba63d9c86bb35e051085
Eaglorn <lexus1.45@mail.ru>
2025-02-01 20:01:23 +1000

    Update ClientApplication.kt

--- src/main/kotlin/ClientApplication.kt
+++ src/main/kotlin/ClientApplication.kt
@@ -1,14 +1,12 @@
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
 import javafx.application.Application
 import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
-import io.ktor.network.selector.SelectorManager
-import io.ktor.network.sockets.aSocket
-import io.ktor.network.sockets.openReadChannel
-import io.ktor.network.sockets.openWriteChannel
-import io.ktor.utils.io.readUTF8Line
-import io.ktor.utils.io.writeStringUtf8
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -17,7 +15,7 @@ import kotlin.system.exitProcess
 class ClientApplication : Application() {
     override fun start(stage: Stage) {
         runBlocking {
-            launch() {
+            launch {
                 val fxmlLoader = FXMLLoader(ClientApplication::class.java.getResource("application-view.fxml"))
                 val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
                 stage.title = "Hello!"
@@ -31,7 +29,7 @@ class ClientApplication : Application() {
             val receiveChannel = socket.openReadChannel()
             val sendChannel = socket.openWriteChannel(autoFlush = true)
 
-            launch(Dispatchers.IO) {
+            CoroutineScope(Dispatchers.IO).launch {
                 while (true) {
                     val greeting = receiveChannel.readUTF8Line()
                     if (greeting != null) {
@@ -45,7 +43,7 @@ class ClientApplication : Application() {
                 }
             }
 
-            launch {
+            CoroutineScope(Dispatchers.Default).launch {
                 while (true) {
                     val myMessage = readln()
                     sendChannel.writeStringUtf8("$myMessage\n")
