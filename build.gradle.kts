65005d482b5be17fabc30ee06d57c6ecb0a1ef3e
Eaglorn <lexus1.45@mail.ru>
2025-04-25 15:10:19 +1000

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.ktor.plugin") version "3.0.3"
     id("org.openjfx.javafxplugin") version "0.1.0"
     id("org.beryx.runtime") version "1.13.1"
+    id("com.google.protobuf") version "0.9.5"
 }
 
 group = "ru.eaglorn.csc"
@@ -27,23 +28,27 @@ kotlin {
     jvmToolchain(23)
 }
 
-val ktor_version = "3.0.3"
-val kotlinx_coroutines_version = "1.10.1"
-val logback_version = "1.5.16"
-val kotlin_version = "2.1.10"
+val ktorVersion = "3.0.3"
+val kotlinxCoroutinesVersion = "1.10.1"
+val logbackVersion = "1.5.16"
+val kotlinVersion = "2.1.10"
+val protobufVersion = "4.30.2"
+val zstdVersion = "1.5.7-2"
+val controlsfxVersion = "11.2.1"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinx_coroutines_version}")
-    implementation("io.ktor:ktor-network:${ktor_version}")
-    implementation("io.ktor:ktor-network-tls:${ktor_version}")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.controlsfx:controlsfx:11.2.1")
-    implementation("eu.hansolo:tilesfx:21.0.9")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+    implementation("io.ktor:ktor-network:$ktorVersion")
+    implementation("io.ktor:ktor-network-tls:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.controlsfx:controlsfx:$controlsfxVersion")
+    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    implementation("com.github.luben:zstd-jni:$zstdVersion")
 }
 
 javafx {
-    version = "23.0.2"
+    version = "24.0.1"
     modules("javafx.controls", "javafx.fxml", "javafx.web", "javafx.swing")
 }
 
@@ -72,5 +77,22 @@ runtime {
         installerOptions.add("--win-dir-chooser")
         installerOptions.add("--win-menu")
         installerOptions.add("--win-shortcut")
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/proto")
+        }
+        java {
+            srcDir("build/generated/source/proto/main/java")
+        }
     }
 }
--- src/main/kotlin/ClientApplication.kt
+++ src/main/kotlin/ClientApplication.kt
@@ -6,11 +6,21 @@ import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.system.exitProcess
+import com.github.luben.zstd.Zstd
+import io.ktor.network.selector.SelectorManager
+import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
+import io.ktor.utils.io.readFully
+import io.ktor.utils.io.readInt
+import io.ktor.utils.io.writeFully
+import kotlinx.coroutines.Dispatchers
+import ru.eaglorn.Message.ChatMessage
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+import kotlin.compareTo
 
 class ClientApplication : Application() {
     override fun start(stage: Stage) {
@@ -22,33 +32,40 @@ class ClientApplication : Application() {
                 stage.scene = scene
                 stage.show()
             }
-
+            
             val selectorManager = SelectorManager(Dispatchers.IO)
             val socket = aSocket(selectorManager).tcp().connect("127.0.0.1", 9002)
+            println("Connected to server")
 
             val receiveChannel = socket.openReadChannel()
             val sendChannel = socket.openWriteChannel(autoFlush = true)
 
-            CoroutineScope(Dispatchers.IO).launch {
-                while (true) {
-                    val greeting = receiveChannel.readUTF8Line()
-                    if (greeting != null) {
-                        println(greeting)
-                    } else {
-                        println("Server closed a connection")
-                        socket.close()
-                        selectorManager.close()
-                        exitProcess(0)
-                    }
-                }
-            }
+            val compressedWelcome = ByteArray(1024)
+            val welcomeSize = receiveChannel.readAvailable(compressedWelcome, 0, compressedWelcome.size)
+            val decompressedWelcome = Zstd.decompress(compressedWelcome, 1024)
+            val welcomeMessage = ChatMessage.parseFrom(decompressedWelcome)
+            println("Server: ${welcomeMessage.message}")
 
-            CoroutineScope(Dispatchers.Default).launch {
-                while (true) {
-                    val myMessage = readln()
-                    sendChannel.writeStringUtf8("$myMessage\n")
-                }
-            }
+            val userName = "ClientUser"
+            val userMessage = ChatMessage.newBuilder()
+                .setName(userName)
+                .setMessage("Hello, server!")
+                .build()
+
+            val compressedMessage = Zstd.compress(userMessage.toByteArray())
+            val messageSizeBytes = ByteBuffer.allocate(4).putInt(compressedMessage.size).array()
+            sendChannel.writeFully(messageSizeBytes)
+            sendChannel.writeFully(compressedMessage)
+
+            val responseSize = receiveChannel.readInt()
+            val compressedResponse = ByteArray(responseSize)
+            receiveChannel.readFully(compressedResponse)
+
+            val decompressedResponse = Zstd.decompress(compressedResponse, 1024)
+            val responseMessage = ChatMessage.parseFrom(decompressedResponse)
+            println("Server: ${responseMessage.message}")
+
+            socket.close()
         }
     }
 
